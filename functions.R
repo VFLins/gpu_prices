4c7bfbc2c8c4dc906e326df13d07776a2c4cb8fe
Vitor Ferreira Lins <vflins@live.com>
2022-05-15 08:26:34 -0300

    tests

--- functions.R
+++ functions.R
@@ -9,24 +9,17 @@ catHeader <- function(text = "", level = 3) {
 allowedDates <- function(months = 12){
     require(zoo)
     
-    allowedMonths <- {as.yearmon( Sys.Date() ) - seq(from = 0, length.out = months, by = 1/12)}
-    allowedMonths <- as.Date(allowedMonths)[order(allowedMonths)]
-    if (Sys.Date() > max(allowedMonths)) {
-        allowedMonths[length(allowedMonths)] <- Sys.Date()
-    }
+    months <- as.integer(months)
+    if (months < 1) {months <- as.integer(1)}
     
-    dates <- c()
-    for (ind in seq_along(allowedMonths)[-1]) {
-        dates <- rbind(
-            dates, 
-            seq.Date(
-                from = allowedMonths[ind-1], 
-                to = allowedMonths[ind], 
-                by = "day"
-            )
-        )
-    }
-    as.Date(dates)
+    firstMonth <- {as.yearmon( Sys.Date() ) - (months - 1) * 1/12}
+    firstMonth <- as.Date(firstMonth)
+
+    dates <- seq.Date(
+        from = firstMonth, 
+        to =  Sys.Date(), 
+        by = "day")
+    dates
 }
 
 priceByStore <- function(data){
