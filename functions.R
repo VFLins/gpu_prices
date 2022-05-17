ea4212e869e2df37f37eee319e8303d86275a3dd
Vitor Ferreira Lins <vflins@live.com>
2022-05-17 00:15:40 -0300

    more tests

--- functions.R
+++ functions.R
@@ -1,8 +1,20 @@
+color_palette <- c("#2196f3", "#234d66")
+
 # Dinamically generate headers for tabsets
 catHeader <- function(text = "", level = 3) {
-    cat(paste0("\n\n", 
-               paste(rep("#", level), collapse = ""), 
-               " ", text, "\n"))
+    cat(paste0(
+        "\n\n", 
+        paste(rep("#", level), collapse = ""), 
+        " ", text, "\n\n"))
+}
+
+# Dinamically generate blocks of code for tabsets
+catCodeBlock <- function(code){
+    cat(paste0(
+        "\n\n`r ",
+        code,
+        "`\n")
+    )
 }
 
 # Return a vector with all the accepeted dates
@@ -20,6 +32,111 @@ allowedDates <- function(months = 12){
         to =  Sys.Date(), 
         by = "day")
     dates
+}
+
+# Plot average of the medians of the pproducts over time
+plotIndexrOverTime <- function(prind_list){
+    require(shiny)
+    require(dplyr)
+    require(tidyr)
+    require(scales)
+    require(ggplot2)
+    require(plotly)
+    
+    currency <- prind_list[[1]]$Currency[1]
+    
+    agg_table <- data.frame()
+    for (table in PRODUCTS) {
+        agg_table <- rbind(
+            agg_table, 
+            select(prind_list[[table]], Date, Price))
+    }
+    agg_table <- agg_table %>% group_by(Date) %>%
+        summarise(
+            PriceMedian = median(Price), 
+            PriceLow = quantile(Price, 0.2),
+            PriceHigh = quantile(Price, 0.8))
+    
+    plot <- ggplot(agg_table, aes(x = Date)) +
+        theme_minimal(base_size = 14) +
+        scale_y_continuous(
+            labels = scales::dollar_format(
+                prefix = currency, big.mark = ".", decimal.mark = ",")) +
+        labs(x = "", y = "")
+    
+    if (nrow(agg_table) > 1) {
+        plot <- plot + 
+            geom_ribbon(aes(
+                ymin = PriceLow, 
+                ymax = PriceHigh), 
+                alpha = 0.33,
+                fill = color_palette[2]) +
+            geom_line(
+                aes(y = PriceMedian), 
+                color = color_palette[1],
+                size = 1.6)
+    } else {
+        plot <- plot +
+            geom_errorbar(aes(
+                ymin = PriceLow, 
+                ymax = PriceHigh), 
+                alpha = 0.33,
+                color = color_palette[2])
+    }
+    plot <-  plot +
+        geom_point(aes(y = PriceMedian), color = color_palette[1], size = 4)
+    ggplotly(plot)
+}
+
+# Plot progression of the price with time
+plotPriceOverTime <- function(prind_table){
+    require(shiny)
+    require(dplyr)
+    require(tidyr)
+    require(scales)
+    require(ggplot2)
+    require(plotly)
+    
+    currency <- prind_table$Currency[1]
+    
+    new_table <- prind_table %>%
+        select(Price, Date) %>%
+        group_by(Date) %>%
+        summarise(
+            PriceMedian = median(Price), 
+            PriceLow = quantile(Price, 0.2),
+            PriceHigh = quantile(Price, 0.8))
+    
+    plot <- ggplot(new_table, aes(x = Date)) +
+        theme_minimal(base_size = 14) +
+        scale_y_continuous(
+            labels = scales::dollar_format(
+                prefix = currency, big.mark = ".", decimal.mark = ",")) +
+        labs(x = "", y = "")
+    
+    if (nrow(new_table) > 1) {
+        plot <- plot + 
+            geom_ribbon(aes(
+                ymin = PriceLow, 
+                ymax = PriceHigh), 
+                alpha = 0.33,
+                fill = color_palette[2]) +
+            geom_line(
+                aes(y = PriceMedian), 
+                color = color_palette[1],
+                size = 1.6)
+    } else {
+        plot <- plot +
+            geom_errorbar(aes(
+                ymin = PriceLow, 
+                ymax = PriceHigh), 
+                alpha = 0.33,
+                color = color_palette[2])
+    }
+    plot <-  plot +
+        geom_point(aes(y = PriceMedian), color = color_palette[1], size = 4)
+    
+    print(plot)
 }
 
 priceByStore <- function(data){
