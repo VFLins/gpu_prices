4fc28d5d1feb5a4fbe752e75b3cd3bbaacb19363
Vitor Ferreira Lins <vflins@live.com>
2022-05-18 07:27:11 -0300

    update on tables

--- functions.R
+++ functions.R
@@ -8,24 +8,6 @@ catHeader <- function(text = "", level = 3) {
         " ", text, "\n\n"))
 }
 
-# Dinamically generate blocks of code for tabsets
-catCodeBlock <- function(code){
-    cat(paste0(
-        "\n\n`r ",
-        eval(cat(code)),
-        "`\n")
-    )
-}
-
-get_h <- function() {
-    with(knitr::opts_current$get(c("fig.height", "dpi", "fig.retina")),
-         fig.height*dpi/fig.retina)
-}
-get_w <- function() {
-    with(knitr::opts_current$get(c("fig.width", "dpi", "fig.retina")),
-        fig.width*dpi/fig.retina)
-}
-
 # Return a vector with all the accepeted dates
 allowedDates <- function(months = 12){
     require(zoo)
@@ -35,7 +17,6 @@ allowedDates <- function(months = 12){
     
     firstMonth <- {as.yearmon( Sys.Date() ) - (months - 1) * 1/12}
     firstMonth <- as.Date(firstMonth)
-
     dates <- seq.Date(
         from = firstMonth, 
         to =  Sys.Date(), 
@@ -43,37 +24,23 @@ allowedDates <- function(months = 12){
     dates
 }
 
-# Plot average of the medians of the products over time
-plotIndexrOverTime <- function(prind_list){
-    require(shiny)
-    require(dplyr)
-    require(tidyr)
-    require(scales)
-    require(ggplot2)
-    require(plotly)
+# Utility function to print prices over time
+timePlot <- function(data, currency) {
+    shureThing <- c("PriceMedian", "PriceLow", "PriceHigh", "Date")
+    # must contain these 4 variables
+    for (var in shureThing) {
+        if (!{var %in% names(data)}) {
+            stop("Necessary variables not present...")
+    }}
     
-    currency <- prind_list[[1]]$Currency[1]
-    
-    agg_table <- data.frame()
-    for (table in PRODUCTS) {
-        agg_table <- rbind(
-            agg_table, 
-            select(prind_list[[table]], Date, Price))
-    }
-    agg_table <- agg_table %>% group_by(Date) %>%
-        summarise(
-            PriceMedian = median(Price), 
-            PriceLow = quantile(Price, 0.2),
-            PriceHigh = quantile(Price, 0.8))
-    
-    plot <- ggplot(agg_table, aes(x = Date)) +
+    plot <- ggplot(data, aes(x = Date)) +
         theme_minimal(base_size = 14) +
         scale_y_continuous(
             labels = scales::dollar_format(
                 prefix = currency, big.mark = ".", decimal.mark = ",")) +
         labs(x = "", y = "")
     
-    if (nrow(agg_table) > 1) {
+    if (nrow(data) > 1) {
         plot <- plot + 
             geom_ribbon(aes(
                 ymin = PriceLow, 
@@ -94,11 +61,36 @@ plotIndexrOverTime <- function(prind_list){
     }
     plot <-  plot +
         geom_point(aes(y = PriceMedian), color = color_palette[1], size = 4)
-    ggplotly(plot, height = get_h(), width = get_w())
+    ggplotly(plot)
+}
+
+# Plot average of the medians of the products over time
+plotIndexr <- function(prind_list){
+    require(shiny)
+    require(dplyr)
+    require(tidyr)
+    require(scales)
+    require(ggplot2)
+    require(plotly)
+    
+    currency <- prind_list[[1]]$Currency[1]
+    
+    agg_table <- data.frame()
+    for (table in PRODUCTS) {
+        agg_table <- rbind(
+            agg_table, 
+            select(prind_list[[table]], Date, Price))}
+    agg_table <- agg_table %>% group_by(Date) %>%
+        summarise(
+            PriceMedian = median(Price), 
+            PriceLow = quantile(Price, 0.2),
+            PriceHigh = quantile(Price, 0.8))
+    
+    timePlot(agg_table, currency)
 }
 
 # Plot progression of the price with time
-plotPriceOverTime <- function(prind_table){
+plotPrice <- function(prind_table){
     require(shiny)
     require(dplyr)
     require(tidyr)
@@ -116,42 +108,29 @@ plotPriceOverTime <- function(prind_table){
             PriceLow = quantile(Price, 0.2),
             PriceHigh = quantile(Price, 0.8))
     
-    plot <- ggplot(new_table, aes(x = Date)) +
-        theme_minimal(base_size = 14) +
-        scale_y_continuous(
-            labels = scales::dollar_format(
-                prefix = currency, big.mark = ".", decimal.mark = ",")) +
-        labs(x = "", y = "")
-    
-    if (nrow(new_table) > 1) {
-        plot <- plot + 
-            geom_ribbon(aes(
-                ymin = PriceLow, 
-                ymax = PriceHigh), 
-                alpha = 0.33,
-                fill = color_palette[2]) +
-            geom_line(
-                aes(y = PriceMedian), 
-                color = color_palette[1],
-                size = 1.6)
-    } else {
-        plot <- plot +
-            geom_errorbar(aes(
-                ymin = PriceLow, 
-                ymax = PriceHigh), 
-                alpha = 0.33,
-                color = color_palette[2])
-    }
-    plot <-  plot +
-        geom_point(aes(y = PriceMedian), color = color_palette[1], size = 4)
-    
-    ggplotly(plot)
+    timePlot(new_table, currency)
 }
 
-priceByStore <- function(data){
+priceByStore <- function(prind_list){
     require(kableExtra)
+    require(stringr)
     require(dplyr)
     require(tidyr)
     
-    kableExtra::kable(data)
+    agg_table <- data.frame()
+    for (table in PRODUCTS) {
+        agg_table <- rbind(
+            agg_table, 
+            select(prind_list[[table]], Date, Price, Store))}
+    agg_table <- agg_table %>% .[.$Date == max(.$Date), ] %>%
+        mutate(Store = str_to_upper(Store))
+
+    stores <- unique(agg_table$Store)
+    output <- list()
+    for (store in stores) {
+        output[[store]] <- agg_table %>% 
+            .[.$Store == store, ] %>%
+            .[order(.$Price, decreasing = FALSE), ] %>%
+            head(., n = 5)}
+    output
 }
