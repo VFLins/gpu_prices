1a36696bd07ffcb37e89981e7f0d4536b544f05f
Vitor F. Lins <vflins@live.com>
2025-02-05 13:37:36 -0300

    add availability index prototype

--- .R
+++ .R
@@ -1,13 +1,37 @@
 library(txtplot)
 PROJECT_ROOT = here::here()
 
-PRICES0 <- readRDS(here::here("backend", "data", "prices.Rds"))
+#PRICES0 <- readRDS(here::here("backend", "data", "prices.Rds"))
 source(here::here("frontend", "scripts", "setup_data.R"))
 
-prices_counts <- sort(table(PRICES[, 3]))
+prices_counts <- sort(table(PRICES$ProductName))
+
 prices_counts_percent <- (
     prices_counts / sum(prices_counts)
 ) *100
+
+products_per_name <- aggregate(
+    x=PRICES$ProductId,
+    by=list(PRICES$ProductName),
+    FUN=function(x)length(unique(x))
+)
+colnames(products_per_name) <- c("Name", "ProductCount")
+names_vec <- products_per_name$Name
+products_per_name <- products_per_name$ProductCount
+names(products_per_name) <- names_vec
+
+prices_counts_per_product <- c()
+for (name in names(prices_counts)) {
+    prices_counts_per_product <- append(
+        prices_counts_per_product,
+        prices_counts[name] / products_per_name[name]
+    )
+}
+availability_index <- sort(
+    prices_counts_per_product
+    - median(prices_counts_per_product)
+    + sd(prices_counts_per_product) * 0.2
+)
 
 # chip_names <- sort(unique(PRICES[, 3]))
 # display_data <- c()
@@ -40,8 +64,3 @@ prices_counts_percent <- (
 # threshold <- mean(display_data)
 # print("low availability")
 # print(display_data[display_data > threshold])
-
-PRICES0[
-    (PRICES$Price < 2200) &
-    (PRICES$ProductName == "Geforce Rtx 4060 Ti"),
-    c("Name", "Store")]
